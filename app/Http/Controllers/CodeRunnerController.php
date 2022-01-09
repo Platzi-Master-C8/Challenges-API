576dd6352d88c8228ac828608eb8b03bfd0ea56a
caprun <26richardr@gmail.com>
2022-01-09 16:40:04 -0600

    feat: Hardcoded Code Runner
    We have an endpoint to run jest test with code written in frontend
    
    Todo:
    Clean test.json returned by code runner
    Bind challenges path with user and challenge.
    Refactor everything in code runner and make it something adaptable to every user and challenge

--- app/Http/Controllers/CodeRunnerController.php
+++ app/Http/Controllers/CodeRunnerController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CodeRunnerController extends Controller
 {
@@ -10,17 +12,28 @@ class CodeRunnerController extends Controller
     public function index()
     {
         try {
-            $out = 'cat';
-//            $out = shell_exec('docker run -d --name docker-user_name challenges/node');
+            $docker_id = shell_exec('docker run -d --name docker-user_name  -v ' . storage_path() . '/app/ChallengesTests/javascript' . ':/usr/src/app/tests challenges/node');
+
+            $out = Storage::disk('local')->get('ChallengesTests/javascript/cats_0151/template.js');
         } catch (\Exception $e) {
             $out = $e->getMessage();
         }
-        return view('codeRunner', ['output' => $out]);
+
+        return view('codeRunner', ['template' => $out]);
     }
 
-    public function runNode()
+    public function runNode(Request $request)
     {
-        $out = shell_exec('docker ps -a');
-        return $out;
+        Storage::disk('local')->put('ChallengesTests/javascript/cats_0151/user_1564/user_result.js', $request->code);
+
+
+        $route = "docker exec docker-user_name  sh -c 'npm run test tests/cats_0151/user_1564/func_0151_1564.test.js -- --json  > tests/cats_0151/user_1564/test.json'";
+        shell_exec($route);
+        try {
+            $result = Storage::disk('local')->get('ChallengesTests/javascript/cats_0151/user_1564/test.json');
+        } catch (FileNotFoundException $e) {
+            $result = $e->getMessage();
+        }
+        return view('codeRunner', ['template' => $result]);
     }
 }
--- resources/views/codeRunner.blade.php
+++ resources/views/codeRunner.blade.php
@@ -1,10 +1,10 @@
 Hi, docker is just running!
 
-{{  $output  }}
 
-
+{{-- @endsection --}}
 <form action="{{route('run_node')}}" method="post">
+    @csrf
     <label for="code">Code</label>
-    <textarea name="code" id="code" cols="60" rows="15" placeholder="type your code here"></textarea>
+    <textarea name="code" id="code" cols="60" rows="15" placeholder="type your code here">{{$template}}</textarea>
     <input type="submit" value="Run">
 </form>
