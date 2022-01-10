353426745de7d5daef708efe0e483d64a3e01240
caprun <26richardr@gmail.com>
2022-01-09 18:32:57 -0600

    feat: Code runner now return a json with test data
    Code runner return the entire JSON file generated by JEST
    
    TODO:
    Adapt docker execution to each user
    
    Issues:
    Change default permission of files generated by docker

--- app/Http/Controllers/CodeRunnerController.php
+++ app/Http/Controllers/CodeRunnerController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class CodeRunnerController extends Controller
 {
-    //Lets start docker if
+    //Start docker if not running
     public function index()
     {
         try {
-            $docker_id = shell_exec('docker run -d --name docker-user_name  -v ' . storage_path() . '/app/ChallengesTests/javascript' . ':/usr/src/app/tests challenges/node');
 
+            // THis docker Id could help us a lot in the future to save the docker id of each user
+            $docker_id = shell_exec('docker run -d --name docker-user_name  -v ' . storage_path() . '/app/ChallengesTests/javascript' . ':/usr/src/app/tests challenges/node');
             $out = Storage::disk('local')->get('ChallengesTests/javascript/cats_0151/template.js');
+            //Todo: Reformat to adapt it with user and resolved challenge
+
         } catch (\Exception $e) {
             $out = $e->getMessage();
         }
@@ -24,16 +26,52 @@ class CodeRunnerController extends Controller
 
     public function runNode(Request $request)
     {
-        Storage::disk('local')->put('ChallengesTests/javascript/cats_0151/user_1564/user_result.js', $request->code);
+//        $user_id = $request->user()->challenger->id;
+//        $challengeId = $request->challenge_id;
+//        $challengeName = $request->challenge_name;
 
-
-        $route = "docker exec docker-user_name  sh -c 'npm run test tests/cats_0151/user_1564/func_0151_1564.test.js -- --json  > tests/cats_0151/user_1564/test.json'";
-        shell_exec($route);
         try {
+            //Storage path will change when we will be using sessions, check NOTION folder structure defined to understand this at all.
+            Storage::disk('local')->put('ChallengesTests/javascript/cats_0151/user_1564/user_result.js', $request->code);
+            //Todo: Reformat adapt it with user and resolved challenge
+
+        } catch (\Exception $e) {
+
+
+            return response()->json(['error' => $e->getMessage()]);
+        }
+        // Run specified test from docker and redirect output to a file, that file have to be binded with local storage
+        $command = "docker exec docker-user_name  sh -c 'npm run test tests/cats_0151/user_1564/func_0151_1564.test.js -- --json  > tests/cats_0151/user_1564/test.json'";
+        //Todo: Reformat adapt it with user and resolved challenge
+
+        shell_exec($command);
+
+
+        try {
+            $path = storage_path() . '/app/ChallengesTests/javascript/cats_0151/user_1564/test.json';
+
+            // npm run jest -- --json return a string with 4 extra unnecessary lines, just trim them
+            $this->trimline($path, 4);
             $result = Storage::disk('local')->get('ChallengesTests/javascript/cats_0151/user_1564/test.json');
-        } catch (FileNotFoundException $e) {
+        } catch (\Exception $e) {
             $result = $e->getMessage();
         }
-        return view('codeRunner', ['template' => $result]);
+
+
+        return view('codeRunner', ['json' => json_decode($result),
+            'template' => $request->code]);
+    }
+
+    /**
+     * @param $file : path to the file to trim
+     * @param $trim : number of lines to trim
+     * @return void
+     * Trim first $trim lines from the $file
+     */
+    private function trimLine($file, $trim)
+    {
+        $lines = file($file);
+        $content = array_slice($lines, $trim);
+        file_put_contents($file, $content);
     }
 }
--- resources/views/codeRunner.blade.php
+++ resources/views/codeRunner.blade.php
@@ -1,10 +1,39 @@
-Hi, docker is just running!
-
-
+<style>
+    html {
+        background-color: #2d2d2d;
+        color: white;
+    }
+</style>
 {{-- @endsection --}}
+
+<h1>Gethired Code Playground</h1>
+
+<p>Write a function that will return the sum of two numbers</p>
 <form action="{{route('run_node')}}" method="post">
     @csrf
-    <label for="code">Code</label>
-    <textarea name="code" id="code" cols="60" rows="15" placeholder="type your code here">{{$template}}</textarea>
-    <input type="submit" value="Run">
+
+    <input type="hidden" name="challenge_id" value="01">
+    <input type="hidden" name="challenge_name" value="cats">
+    <label for="code"></label>
+    @if(isset($json) && $json->testResults[0]->assertionResults[0]->status == 'passed' )
+        <textarea name="code" id="code" cols="60" rows="15" placeholder="type your code here"
+                  disabled>{{$template}}</textarea>
+
+    @else
+        <textarea name="code" id="code" cols="60" rows="15" placeholder="type your code here">{{$template}}</textarea>
+        <input type="submit" value="Run">
+
+    @endif
+
+
 </form>
+
+@php
+    echo '<pre>';
+
+@endphp
+
+@if(isset($json))
+    <h4> Status: {{$json->testResults[0]->assertionResults[0]->status}}</h4>
+@endif
+
