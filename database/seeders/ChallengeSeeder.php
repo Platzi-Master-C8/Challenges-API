a8be485bafe7b20642cb011940dbe3dd0a544c1d
caprun <26richardr@gmail.com>
2022-02-01 14:04:33 -0600

    feat: deployment succesfull

--- database/seeders/ChallengeSeeder.php
+++ database/seeders/ChallengeSeeder.php
@@ -54,7 +54,9 @@ class ChallengeSeeder extends Seeder
             "description" => "Create a function that returns the sum of a + b",
             "time_out" => 45,
             "difficulty" => ChallengeDifficulties::LOW,
-            'func_template' => "function sum(a,b){\n\t/*Your code here*/\n}" .
+            'func_template' => "
+            const func = require('./user_func.js');
+            function sum(a,b){\n\t/*Your code here*/\n}" .
                 "\nmodule.exports=sum;",
             'test_template' => "test('adds 1 + 2 to equal 3', () => {
     expect(func(1, 2)).toBe(3);
